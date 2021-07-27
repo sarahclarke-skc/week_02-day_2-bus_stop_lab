2714b1cdfa715eac8e9896ddbf53f113468a1004
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-07-27 13:41:06 +0100

    Task A Completed

---
+++ src/bus.py
@@ -0,0 +1,9 @@
+class Bus:
+    def __init__(self, route_number, capacity, destination, price):
+        self.route_number = route_number
+        self.capacity = capacity
+        self.destination = destination
+        self.price = price
+
+    def drive():
+        return "Brum Brum"
---
+++ src/person.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name, age, cash):
+        self.name = name
+        self.age = age
+        self.cash = cash
+
+        
