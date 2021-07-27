2006ea5f61201e03904593ba4a8a91b36ffc983c
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-07-27 14:12:44 +0100

    everything upto picking up passengers from bus stop

--- src/bus.py
+++ src/bus.py
@@ -1,9 +1,22 @@
 class Bus:
-    def __init__(self, route_number, capacity, destination, price):
+    def __init__(self, route_number, destination, price, capacity):
         self.route_number = route_number
         self.capacity = capacity
         self.destination = destination
         self.price = price
+        self.passengers =[]
 
-    def drive():
-        return "Brum Brum"
+    def drive(self):
+        return "Brum brum"
+
+    def passenger_count(self):
+        return len(self.passengers)
+
+    def pick_up(self, person):
+        self.passengers.append(person)
+
+    def drop_off(self, person):
+        self.passengers.remove(person)
+
+    def empty(self):
+        self.passengers = []
